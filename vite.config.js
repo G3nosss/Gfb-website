49f2505f1978910dd3c0e54198854d1637f728d9
Aman Sarma <aman@pragjyotishcollege.ac.in>
2026-05-02 22:33:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Gfb-website/',
   plugins: [react()],
-  base: '/Gfb-website/',
   assetsInclude: ['**/*.glb', '**/*.gltf'],
 })
